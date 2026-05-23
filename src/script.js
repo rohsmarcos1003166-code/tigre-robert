d17453fe6f1336d8d5bc24336bbfa79c57642dae
rohsmarcos1003166-code <rohsmarcos1003166@gmail.com>
2026-05-23 02:40:46 -0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -4,7 +4,6 @@ window.girarSlots = () => {
     const colunas = document.querySelectorAll('.coluna');
     let giros = 0;
     
-    // Animação de giro
     const intervalo = setInterval(() => {
         colunas.forEach(col => {
             const divs = col.querySelectorAll('.simbolo');
@@ -13,9 +12,19 @@ window.girarSlots = () => {
         giros++;
         if (giros > 15) {
             clearInterval(intervalo);
+            verificarVitoria();
         }
     }, 100);
 };
+
+function verificarVitoria() {
+    // Simulação: 30% de chance de ganhar para teste
+    if (Math.random() > 0.7) {
+        const msg = document.getElementById('notificacao');
+        msg.classList.remove('hidden');
+        setTimeout(() => msg.classList.add('hidden'), 3000);
+    }
+}
 
 window.alterarAposta = (mod) => {
     const display = document.getElementById("valor-aposta");
